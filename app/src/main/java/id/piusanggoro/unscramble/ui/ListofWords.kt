ed53e10faf7323c03eaf579e759d794791e0a865
Pius Anggoro <piusanggoro@gmail.com>
2022-11-13 23:16:42 +0700

    Initial commit

---
+++ app/src/main/java/id/piusanggoro/unscramble/ui/ListofWords.kt
@@ -0,0 +1,7 @@
+package id.piusanggoro.unscramble.ui
+
+const val MAKS_NAMA = 4
+const val SCORE_INCREASE = 25
+
+val allWordsList: List<String> = listOf("trex", "bronto",
+    "stego", "trice", "raptor")
